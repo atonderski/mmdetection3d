a18059938b464154e4f3b2d79f6ccf7c2d3613cd
Adam Tonderski <adam.tonderski@zenseact.com>
2023-02-22 16:02:30 +0100

    more sophisticated singularity script

--- run_singularity.sh
+++ run_singularity.sh
@@ -7,13 +7,13 @@
 #SBATCH --time 168:00:00
 #SBATCH --mem-per-gpu 64G
 #SBATCH --output /workspaces/%u/mmdetection3d/logs/%j.out
-#SBATCH --partition zprodlow
+#SBATCH --partition zprod
 #
 
-singularity exec --nv --bind /workspaces/$USER:/workspace \
-  --bind /staging/dataset_donation:/staging/dataset_donation \
-  --pwd /workspace/mmdetection3d/ \
-  --env PYTHONPATH=/workspace/mmdetection3d/ \
+singularity exec --nv --bind /workspaces/$USER/mmdetection3d:/mmdetection3d \
+  --bind /staging/dataset_donation/round_2:/mmdetection3d/data/zod \
+  --pwd /mmdetection3d/ \
+  --env PYTHONPATH=/mmdetection3d/ \
   /workspaces/s0000960/mmdetection3d/mmdet3d.sif \
   python3 -u $@
 #
