914c03b640a69a6352c5442fcbfdc15e6650fd2b
Adam Tonderski <adam.tonderski@zenseact.com>
2023-05-25 21:25:15 +0200

    Add script for running distributed

---
+++ run_berzelius.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --nodes 1
+#SBATCH --gpus 4
+#SBATCH --time 1-00:00:00
+#SBATCH --output /proj/nlp4adas/users/%u/logs/%j.out
+#SBATCH -A berzelius-2022-232
+#
+
+rm -f /proj/nlp4adas/users/$USER/mmdetection3d/data/zod
+
+singularity exec --nv --bind /proj/nlp4adas/users/$USER/mmdetection3d:/mmdetection3d \
+  --bind /proj/adas-data/data/zod:/mmdetection3d/data/zod \
+  --pwd /mmdetection3d/ \
+  --env PYTHONPATH=/mmdetection3d/ \
+  /proj/nlp4adas/containers/mmdet3d.sif \
+  bash tools/dist_train.sh $1 4 ${@:2}
+#
+#EOF
