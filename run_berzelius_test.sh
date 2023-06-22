1c600778575efb39dea02f1f3b0224c902dcae26
Adam Tonderski <adam.tonderski@zenseact.com>
2023-06-22 14:50:56 +0200

    sbatch scripts

--- run_berzelius_test.sh
+++ run_berzelius_test.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --nodes 1
 #SBATCH --gpus 8
-#SBATCH --time 1-00:00:00
+#SBATCH --time 02:00:00
 #SBATCH --output /proj/nlp4adas/users/%u/logs/%j.out
 #SBATCH -A berzelius-2022-232
 #
@@ -11,8 +11,7 @@ singularity exec --nv --bind /proj/nlp4adas/users/$USER/mmdetection3d:/mmdetecti
   --bind /proj/adas-data/data/zod:/mmdetection3d/data/zod \
   --bind /home/$USER/.local/lib/python3.8/site-packages-mmdet:/home/$USER/.local/lib/python3.8/site-packages \
   --pwd /mmdetection3d/ \
-  --env PYTHONPATH=/mmdetection3d/ \
   /proj/nlp4adas/containers/mmdet3d.sif \
-  bash tools/dist_train.sh $1 8 ${@:2}
+  bash tools/dist_test.sh $1 $2 8 ${@:3}
 #
 #EOF
